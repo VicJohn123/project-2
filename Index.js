0253d0c8c80245bc199afb2230a96358e10da7df
VicJohn123 <148836455+VicJohn123@users.noreply.github.com>
2023-10-24 21:42:51 +0800

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,1 +1,22 @@
-
+const button = document.querySelectorAll("button") 
+const inputField = document.getElementById("results") 
+ 
+   let string="";
+   let array = Array.from(button);
+   array.forEach(button => {
+   button.addEventListener("click", (e) => {
+     
+    if(e.target.innerHTML==="="){
+       string = eval(string);
+       inputField.value = string;
+    }else if(e.target.innerHTML==="C"){
+       string="";
+       inputField.value = string;
+    }else if(e.target.innerHTML==="Del"){
+     string = string.substring(0, string.length-1);
+     inputField.value = string;
+    }else{
+      string += e.target.innerHTML;
+      inputField.value = string;
+    }
+  });
